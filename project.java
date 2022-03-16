6ea61a2ddecc8e7597620288d44c49642836584f
ngoc anh <ngocanh0202@gmail.com>
2022-03-16 22:18:26 +0700

    Them ham chuc nang cua du an

--- project.java
+++ project.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
 
 public class project {
+    static void tinhtong(int a, int b){
+
+    }
+    static void tinhhieu(int a,int b){
+
+    }
+    static void tinhtich(int a,int b){
+
+    }
+    static void tinhthuong(int a, int b){
+
+    }
+
     public static void main(String[] args) {
         Scanner sv = new Scanner(System.in);
         System.out.println("Nhap a:");
         int a = sv.nextInt();
         System.out.println("Nhap b:");
         int b = sv.nextInt();
-        
     }
 }
