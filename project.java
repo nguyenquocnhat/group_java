a0396d3bbab9685b3553bfa69e276c07d6964151
ngoc anh <ngocanh0202@gmail.com>
2022-03-16 22:11:51 +0700

    Du an dau tien

---
+++ project.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class project {
+    public static void main(String[] args) {
+        Scanner sv = new Scanner(System.in);
+        System.out.println("Nhap a:");
+        int a = sv.nextInt();
+        System.out.println("Nhap b:");
+        int b = sv.nextInt();
+        
+    }
+}
