b2b1de8027363975c39e15f641180e9f97c8ee86
nguyenquocnhat <101856628+nguyenquocnhat@users.noreply.github.com>
2022-03-18 16:35:38 +0700

    Update project.java
    Them chuc nang chia

--- project.java
+++ project.java
@@ -10,9 +10,18 @@ public class project {
     static void tinhtich(int a,int b){
 
     }
-    static void tinhthuong(int a, int b){
-
-    }
+    static float tinhthuong(int a, int b){
+    	int b1 = 0;
+		if(b==0) {
+			do {
+				System.out.println("Nhap lai b :");
+				Scanner objSc = new Scanner(System.in);
+				b1 = objSc.nextInt();
+			}while(b1 == 0);
+			return (float) (a*1.0/b1);
+		}else
+			return (float) ((a*1.0)/b);
+	}
 
     public static void main(String[] args) {
         Scanner sv = new Scanner(System.in);
@@ -20,5 +29,6 @@ public class project {
         int a = sv.nextInt();
         System.out.println("Nhap b:");
         int b = sv.nextInt();
+        System.out.println("Thuong hai so la: " + tinhthuong(a,b));
     }
 }
